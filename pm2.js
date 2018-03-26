de389b581fba1d6dce89e367e19b89dfd7851060
Austin Burdine <acburdine@gmail.com>
2018-03-26 17:16:50 -0400

    feat: update code to work w/latest CLI version
    - fix the hackiness in the isRunning method
    - run ensureStarted on start to make sure we actually start

--- pm2.js
+++ pm2.js
@@ -1,10 +1,10 @@
 'use strict';
-const cli = require('ghost-cli');
+
+const ProcessManager = require('ghost-cli').ProcessManager;
 const Promise = require('bluebird');
 const pm2 = require('pm2');
-const execa = require('execa');
 
-class PM2ProcessManager extends cli.ProcessManager {
+class PM2ProcessManager extends ProcessManager {
   get ghostBinFile() {
     return process.argv[1];
   }
@@ -24,42 +24,43 @@ class PM2ProcessManager extends cli.ProcessManager {
         cwd: cwd
       }, cb));
     }).then(() => {
+      return this.ensureStarted();
+    }).finally(() => {
       return pm2.disconnect();
     });
   }
 
   stop() {
     return this._connect().then(() => {
-      return Promise.fromNode((cb) => pm2.stop(this.instance.name, cb));
-    }).then(() => {
+      return Promise.fromCallback((cb) => pm2.stop(this.instance.name, cb));
+    }).finally(() => {
       return pm2.disconnect();
     });
   }
 
   isRunning() {
-    // TODO: this is really dirty, but it's the only way to get things
-    // to work w/o asynchronous behavior
-    try {
-      let result = execa.sync('pm2', ['show', this.instance.name], {
-        preferLocal: true,
-        localDir: __dirname
-      });
+    return this._connect().then(() => {
+      return Promise.fromCallback((cb) => pm2.describe(this.instance.name, cb));
+    }).then((list) => {
+      const pm2Instance = list.find((l) => l.name === this.instance.name);
 
-      return Boolean(result.stdout.match(/online/));
-    } catch (e) {
-      if (e.code === 1) {
-        // pm2 process doesn't exist, return false
-        return false;
+      if (!pm2Instance) {
+        return Promise.resolve(false);
       }
-    }
+
+      // the pid property is 0 when the process isn't running.
+      return Promise.resolve(pm2Instance.pid > 0);
+    }).finally(() => {
+      pm2.disconnect();
+    });
   }
 
   _connect() {
-    return Promise.fromNode((cb) => pm2.connect(cb));
+    return Promise.fromCallback((cb) => pm2.connect(cb));
   }
 
   /**
-   * TODO: determine whether pm2 will _actually_ run on any system :p
+   * TODO: determine whether pm2 will *actually* run on any system :p
    */
   static willRun() {
     return true;
